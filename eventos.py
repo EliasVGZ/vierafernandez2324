5a25ed7f9e1ea2f56f7a75e578d2692c52af331f
Elias <elias.bs2@hotmail.com>
2023-10-02 13:39:23 +0200

    boton

--- eventos.py
+++ eventos.py
@@ -3,7 +3,7 @@ import var
 class Eventos():
     def saludar(self):
         try:
-            var.ui.lblTitulo.setText("hola, haz pulsado el boton")
+            var.ui.lblTitulo.setText("Hola, haz pulsado el boton")
 
         except Exception as error:
             print(error, "en modulos eventos")
