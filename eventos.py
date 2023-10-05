a232d1d6141e0d6f558d7cacf8d9dd3716d5d0cf
Elias <elias.bs2@hotmail.com>
2023-10-05 14:20:36 +0200

    empezamos con ventana salir y ventana acerca de

--- eventos.py
+++ eventos.py
@@ -1,9 +1,25 @@
-import var
+import var, sys
 
 class Eventos():
-    def saludar(self):
+    @staticmethod
+    def salir(self):
         try:
-            pass
+            sys.exit()
 
         except Exception as error:
             print(error, "en modulos eventos")
+
+    @staticmethod
+    def abrirCalendario(self):
+        try:
+            var.calendar.show()
+        except Exception as error:
+            print("error en abrir calendario", error)
+
+
+    @staticmethod
+    def acercaDe(self):
+        try:
+            pass
+        except Exception as error:
+            print ("error abrir ventana acerca de", error)
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+from datetime import datetime
 
 from PyQt6 import QtWidgets
 
 import eventos
 from venta_principal import *
 import sys, var
+from calendario import *
+
+
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
         var.ui = Ui_VentanaPrincipal()
         var.ui.setupUi(self) #encargado de la interfaz
+        var.calendar=Calendar()
 
-        '''
-        zona de eventos de botones
-        
-        '''
+        """zona de eventos de botones"""
+
+        var.ui.btnCalendario.clicked.connect(eventos.Eventos.abrirCalendario)
+
+        """ ZONA DE EVENTOS DEL MENU BAR"""
+
+        var.ui.actionSalir.triggered.connect(eventos.Eventos.salir)
+
+
+
+class Calendar(QtWidgets.QDialog):
+    def __init__(self):
+        super(Calendar, self). __init__()
+        var.calendar = Ui_Calendario()
+        var.calendar.setupUi(self)
+        dia= datetime.now().day
+        mes = datetime.now().month
+        ano = datetime.now().year
 
 
 
